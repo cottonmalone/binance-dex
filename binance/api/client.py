890f4d0ac9d39b419c6ceedf3d6cd183c5f14f65
Cotton Malone <cotton.malone@mail.com>
2019-04-01 13:38:22 +0100

    Adding simple API calls with GET and no parameters.

--- binance/api/client.py
+++ binance/api/client.py
@@ -28,3 +28,43 @@ class Client(object):
 
         """
         return get(self.base_url, '/api/v1/time')
+
+    def get_node_info(self):
+        """
+        Gets runtime information about the node.
+
+        Returns:
+            dict: The response data.
+
+        """
+        return get(self.base_url, '/api/v1/node-info')
+
+    def get_validators(self):
+        """
+        Gets the list of validators used in consensus.
+
+        Returns:
+            list(dict): The response data.
+
+        Raises:
+            BadRequest: If the input is malformed.
+            NotFound: If the resource could not be found.
+            UnknownError: For any unexpected error.
+
+        """
+        return get(self.base_url, '/api/v1/validators')
+
+    def get_peers(self):
+        """
+        Gets the list of network peers.
+
+        Returns:
+            list(dict): The response data.
+
+        Raises:
+            BadRequest: If the input is malformed.
+            NotFound: If the resource could not be found.
+            UnknownError: For any unexpected error.
+
+        """
+        return get(self.base_url, '/api/v1/peers')
--- tests/api/test_client.py
+++ tests/api/test_client.py
@@ -20,3 +20,45 @@ def test_client_get_time(mocker):
 
     # check that mock functions were called as expected
     get.assert_called_with('url', '/api/v1/time')
+
+
+def test_client_get_node_info(mocker):
+    """
+    Test that methods behaves as expected.
+    """
+    # mock all underlying functionality
+    get = mocker.patch('binance.api.client.get', return_value='foo')
+
+    # check that return value is correct
+    assert Client('url').get_node_info() == 'foo'
+
+    # check that mock functions were called as expected
+    get.assert_called_with('url', '/api/v1/node-info')
+
+
+def test_client_get_validators(mocker):
+    """
+    Test that methods behaves as expected.
+    """
+    # mock all underlying functionality
+    get = mocker.patch('binance.api.client.get', return_value='foo')
+
+    # check that return value is correct
+    assert Client('url').get_node_info() == 'foo'
+
+    # check that mock functions were called as expected
+    get.assert_called_with('url', '/api/v1/validators')
+
+
+def test_client_get_peers(mocker):
+    """
+    Test that methods behaves as expected.
+    """
+    # mock all underlying functionality
+    get = mocker.patch('binance.api.client.get', return_value='foo')
+
+    # check that return value is correct
+    assert Client('url').get_node_info() == 'foo'
+
+    # check that mock functions were called as expected
+    get.assert_called_with('url', '/api/v1/peers')
