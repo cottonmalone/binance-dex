49a898c41f0f39b64c71abf5a0b1b3dd362f9c5f
Cotton Malone <cotton.malone@mail.com>
2019-04-05 16:23:13 +0100

    Adding crypto module for keys.

---
+++ binance/crypto/keys.py
@@ -0,0 +1,205 @@
+import binascii
+import bitcoin
+import ecdsa
+import hashlib
+from binance.utils import segwit_addr
+
+
+def get_private_key_from_hex(hex):
+    """
+    Get private key from hexadecimal representation.
+
+    Args:
+        hex (str): The hexadecimal string.
+
+    Returns:
+        ecdsa.SigningKey: The private key.
+
+    """
+    return ecdsa.SigningKey.from_string(binascii.unhexlify(hex),
+                                        curve=ecdsa.SECP256k1)
+
+
+def get_hex_from_private_key(key):
+    """
+    Get hexadecimal representation of private key.
+
+    Args:
+        key (ecdsa.SigningKey): The private key.
+
+    Returns:
+        str: The hexadecimal string.
+
+    """
+    return binascii.hexlify(key.to_string()).decode()
+
+
+def get_public_key_from_hex(hex):
+    """
+    Get public key from hexadecimal representation.
+
+    Args:
+        hex (str): The hexadecimal string.
+
+    Returns:
+        ecdsa.VerifyingKey: The primary key.
+
+    """
+    return ecdsa.VerifyingKey.from_string(
+        bitcoin.decompress(binascii.unhexlify(hex))[1:],
+        curve=ecdsa.SECP256k1
+    )
+
+
+def get_hex_from_public_key(key):
+    """
+    Get hexadecimal representation of public key.
+
+    Args:
+        key (ecdsa.VerifyingKey): The public key.
+
+    Returns:
+        str: The hexadecimal string.
+
+    """
+    return bitcoin.compress('04' + binascii.hexlify(key.to_string()).decode())
+
+
+def generate_private_key():
+    """
+    Generate random private key.
+
+    Returns:
+        str: The hexadecimal representation of the key.
+
+    """
+    return get_hex_from_private_key(
+        ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)
+    )
+
+
+def get_public_key_from_private_key(private_key):
+    """
+    Get public key from private key.
+
+    Args:
+        private_key (str): The hexadecimal representation of the key.
+
+    Returns:
+        str: The corresponding public key in hexadecimal form.
+
+    """
+    return get_hex_from_public_key(
+        get_private_key_from_hex(private_key).get_verifying_key()
+    )
+
+
+def get_address_from_public_key(public_key, prefix="tbnb"):
+    """
+    Get address from public key.
+
+    Args:
+        public_key (str): The public key in hexadecimal form.
+        prefix (str): The address prefix
+
+    Returns:
+        str: The hexadecimal representation of the address.
+
+    """
+    # get compressed public key (33 bytes from 64)
+    # compressed_key = bitcoin.compress('04' + public_key)
+
+    # get sha256 hash
+    sha256 = hashlib.sha256(binascii.unhexlify(public_key.encode())).digest()
+
+    # get ripemd160 hash
+    ripemd160 = hashlib.new('ripemd160', sha256).digest()
+
+    # convert digest to bytes
+    data = segwit_addr.convertbits(ripemd160, 8, 5)
+
+    # encode using segwit_addr library
+    return segwit_addr.bech32_encode(prefix, data)
+
+
+def get_public_key_from_address(address):
+    """
+    Get public key from address.
+
+    Args:
+        address (str): The hexadecimal representation of the address.
+
+    Returns:
+        str: The public key in hexadecimal form.
+
+    """
+    # get address data
+    _, data = segwit_addr.bech32_decode(address)
+
+    # decode data back to key
+    decoded = segwit_addr.convertbits(data, 5, 8, False)
+
+    # convert byte array to hex
+    public_key = binascii.hexlify(bytearray(decoded)).decode()
+
+    return public_key
+
+
+def get_address_from_private_key(private_key, prefix="tbnb"):
+    """
+    Get address from public key.
+
+    Args:
+        private_key (str): The private key in hexadecimal form.
+        prefix (str): The address prefix (e.g. bnb or tbnb).
+
+    Returns:
+        str: The hexadecimal representation of the address.
+
+    """
+    return get_address_from_public_key(
+        get_public_key_from_private_key(private_key),
+        prefix
+    )
+
+
+def generate_signature_for_message(private_key, message):
+    """
+    Generate signature for message.
+
+    Args:
+        private_key (str): The private key in hexadecimal form.
+        message (bytes): The message to sign in bytes.
+
+    Returns:
+        bytes: The signed message.
+
+    """
+    # get private key
+    key = get_private_key_from_hex(private_key)
+
+    # sign message using sha256
+    return key.sign(message, hashfunc=hashlib.sha256)
+
+
+def verify_signature_for_message(public_key, signature, message):
+    """
+    Verify signature for message.
+
+    Args:
+        public_key (str): The public key in hexadecimal form.
+        signature (bytes): The message to sign in bytes.
+        message (bytes): The message to sign in bytes.
+
+    Returns:
+        bool: Whether the signature was verified.
+
+    """
+    key = get_public_key_from_hex(public_key)
+
+    # this function is annoying as it will return True if verified but
+    # an exception if not, not very good application of good design principles
+    try:
+        return key.verify(signature, message, hashfunc=hashlib.sha256)
+    except ecdsa.BadSignatureError:
+        return False
---
+++ tests/crypto/test_keys.py
@@ -0,0 +1,96 @@
+import binascii
+import pytest
+from binance.crypto import *
+
+"""
+The following keys are taken from the Binance DEX SDK.
+"""
+PRIVATE_KEY = "30c5e838578a29e3e9273edddd753d6c9b38aca2446dd84bdfe2e5988b0da0a1"
+
+MSG = "7b226163636f756e745f6e756d626572223a2231222c22636861696e5f6964223a22626e6" \
+      "2636861696e2d31303030222c226d656d6f223a22222c226d736773223a5b7b226964223a" \
+      "2242363536314443433130343133303035394137433038463438433634363130433146364" \
+      "6393036342d3130222c226f7264657274797065223a322c227072696365223a3130303030" \
+      "303030302c227175616e74697479223a313230303030303030302c2273656e646572223a2" \
+      "2626e63316b6574706d6e71736779637174786e7570723667636572707073306b6c797279" \
+      "687a36667a6c222c2273696465223a312c2273796d626f6c223a224254432d3543345f424" \
+      "e42222c2274696d65696e666f726365223a317d5d2c2273657175656e6365223a2239227d"
+
+SIGNATURE = "9c0421217ef92d556a14e3f442b07c85f6fc706dfcd8a72d6b58f05f96e95aa2" \
+            "26b10f7cf62ccf7c9d5d953fa2c9ae80a1eacaf0c779d0253f1a34afd17eef34"
+
+
+def test_generate_private_key():
+    """
+    Test function behaves as expected.
+    """
+    # just checking that the key is of expected length
+    assert len(generate_private_key()) == 64
+
+
+@pytest.mark.parametrize('key,prefix,address', [
+    ("90335b9d2153ad1a9799a3ccc070bd64b4164e9642ee1dd48053c33f9a3a05e9",
+     "tbnb",
+     "tbnb1hgm0p7khfk85zpz5v0j8wnej3a90w709zzlffd"),
+    ("90335b9d2153ad1a9799a3ccc070bd64b4164e9642ee1dd48053c33f9a3a05e9",
+     "bnc",
+     "bnc1hgm0p7khfk85zpz5v0j8wnej3a90w7098fpxyh")
+])
+def test_get_address_from_both_keys(key, prefix, address):
+    """
+    Test function behaves as expected.
+    """
+    # check that address is correct for private key
+    assert get_address_from_private_key(key, prefix) == address
+
+    # get public key from private key
+    public_key = get_public_key_from_private_key(key)
+
+    # check that address is correct for public key
+    assert get_address_from_public_key(public_key, prefix) == address
+
+
+def test_get_public_key_from_address():
+    """
+    Test function behaves as expected.
+    """
+    assert get_public_key_from_address(
+        "tbnb1hgm0p7khfk85zpz5v0j8wnej3a90w709zzlffd") == "ba36f0fad74d8f41045463e4774f328f4af779e5"
+
+
+def test_generate_signature_for_message():
+    """
+    Test function behaves as expected.
+    """
+    # get binary message
+    message_data = binascii.unhexlify(MSG)
+
+    # generate signature
+    signature = generate_signature_for_message(PRIVATE_KEY,
+                                               message_data)
+
+    # get public key from private key
+    public_key = get_public_key_from_private_key(PRIVATE_KEY)
+
+    # verify that signature is correct
+    assert verify_signature_for_message(public_key, signature, message_data)
+
+
+def test_verify_signature_for_message():
+    """
+    Test function behaves as expected.
+    """
+    # get binary message
+    message_data = binascii.unhexlify(MSG)
+
+    # generate signature
+    signature = binascii.unhexlify(SIGNATURE)
+
+    # get public key from private key
+    public_key = get_public_key_from_private_key(PRIVATE_KEY)
+
+    # verify that signature is correct
+    assert verify_signature_for_message(public_key, signature, message_data)
+
+    # verify that bogus signature is rejected
+    assert not verify_signature_for_message(public_key, signature, b'foo')
